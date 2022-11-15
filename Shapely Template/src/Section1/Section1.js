d259b657a3f2a23c928c4b3250f646036dd8fbab
Mina <minakamali.d@gmail.com>
2022-11-15 14:40:48 +0330

    Section1 finished

--- Shapely Template/src/Section1/Section1.js
+++ Shapely Template/src/Section1/Section1.js
@@ -1,9 +1,15 @@
 import "./Section1.css";
+
 function Section1() {
   return (
     <div>
       <section>
-        <p>We Change Everything WordPress</p>
+        <h1>We Change Everything WordPress</h1>
+        <p>This is the only WordPress theme you will ever want to use.</p>
+        <div className="buttons">
+          <button id="read">READ MORE</button>
+          <button id="download">DOWNLOAD NOW</button>
+        </div>
       </section>
     </div>
   );
