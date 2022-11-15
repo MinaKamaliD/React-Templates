585deab79fcb6222fab57f5c99ec6f7ae0cabf67
Mina <minakamali.d@gmail.com>
2022-11-15 14:49:26 +0330

    Section1 modified

--- Shapely Template/src/App.js
+++ Shapely Template/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import Section1 from "./Section1/Section1";
+import Section2 from "./Section2/Section2";
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Section1></Section1>
+      <Section2></Section2>
     </div>
   );
 }
---
+++ Shapely Template/src/Section2/Section2.js
@@ -0,0 +1,6 @@
+import "./Section2.css";
+
+function Section2() {
+  return <div></div>;
+}
+export default Section2;
