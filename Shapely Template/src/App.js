709741c6cd176a750ceb857b59222bc7d7cdb64f
Mina <minakamali.d@gmail.com>
2022-11-15 13:48:09 +0330

    Header Modified

--- Shapely Template/src/App.js
+++ Shapely Template/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
+import Section1 from "./Section1/Section1";
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Section1></Section1>
     </div>
   );
 }
---
+++ Shapely Template/src/Section1/Section1.js
@@ -0,0 +1,11 @@
+import "./Section1.css";
+function Section1() {
+  return (
+    <div>
+      <section>
+        <p>We Change Everything WordPress</p>
+      </section>
+    </div>
+  );
+}
+export default Section1;
