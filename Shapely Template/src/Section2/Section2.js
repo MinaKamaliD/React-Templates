fc8f86cd977f918bb0a2bf3e4fbdf23bd15abfcb
Mina <minakamali.d@gmail.com>
2022-11-16 09:01:07 +0330

    Section1 modified & Section2 finished

--- Shapely Template/src/Section2/Section2.js
+++ Shapely Template/src/Section2/Section2.js
@@ -1,6 +1,25 @@
 import "./Section2.css";
 
 function Section2() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="img">
+          <img src="../img/MacBook.png" alt="" srcset="" />
+        </div>
+        <div className="content">
+          <h2>SEO Friendly</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            pulvinar luctus sem, eget porta orci. Maecenas molestie dui id diam
+            feugiat, eu tincidunt mauris aliquam. Duis commodo vitae ligula et
+            interdum. Maecenas faucibus mattis imperdiet. In rhoncus ac ligula
+            id ultricies.
+          </p>
+          <button id="btn">READ MORE</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Section2;
