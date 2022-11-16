fb17d4c5620f04bdf3bd5f7e72ef6acbb70d0911
Mina <minakamali.d@gmail.com>
2022-11-16 09:17:10 +0330

    Section3 finished

--- Shapely Template/src/App.js
+++ Shapely Template/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import Section1 from "./Section1/Section1";
 import Section2 from "./Section2/Section2";
+import Section3 from "./Section3/Section3";
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Section1></Section1>
       <Section2></Section2>
+      <Section3></Section3>
     </div>
   );
 }
---
+++ Shapely Template/src/Section3/Section3.js
@@ -0,0 +1,24 @@
+import "./Section3.css";
+
+function Section2() {
+  return (
+    <div>
+      <div className="container-section3">
+        <div className="content">
+          <h2>SEO Friendly</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            pulvinar luctus sem, eget porta orci. Maecenas molestie dui id diam
+            feugiat, eu tincidunt mauris aliquam. Duis commodo vitae ligula et
+            interdum.
+          </p>
+          <button id="btn">READ MORE</button>
+        </div>
+        <div className="img">
+          <img src="../img/3.png" alt="" srcset="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Section2;
