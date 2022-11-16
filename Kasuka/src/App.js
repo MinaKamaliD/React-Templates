4e4896b44a84f532a47ba08b1db0192dea24b278
Mina <minakamali.d@gmail.com>
2022-11-16 13:10:18 +0330

    Parallax finished

--- Kasuka/src/App.js
+++ Kasuka/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Parallax from "./Parallax/Parallax";
 
 function App() {
-  return;
+  return (
+    <div>
+      <Parallax></Parallax>;
+    </div>
+  );
 }
 
 export default App;
---
+++ Kasuka/src/Parallax/Parallax.js
@@ -0,0 +1,11 @@
+import "./Parallax.css";
+
+function Parallax() {
+  return (
+    <div>
+      <div className="parallax"></div>
+    </div>
+  );
+}
+
+export default Parallax;
