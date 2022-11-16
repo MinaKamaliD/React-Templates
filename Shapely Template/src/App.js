dd396389ecfa90b3c912ad6571b8978185d6160d
Mina <minakamali.d@gmail.com>
2022-11-16 10:29:33 +0330

    NavBar modified

--- Shapely Template/src/App.js
+++ Shapely Template/src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./NavBar/NavBar";
 import Section1 from "./Section1/Section1";
 import Section2 from "./Section2/Section2";
 import Section3 from "./Section3/Section3";
+import Section4 from "./Section4/Section4";
 function App() {
   return (
     <div>
@@ -10,6 +11,8 @@ function App() {
       <Section1></Section1>
       <Section2></Section2>
       <Section3></Section3>
+      <Section4></Section4>
+      <Section4></Section4>
     </div>
   );
 }
---
+++ Shapely Template/src/Section4/Section4.js
@@ -0,0 +1,21 @@
+import "./Section4.css";
+
+function Section1() {
+  return (
+    <div>
+      <section>
+        <h1>Small Parallax Section</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          imperdiet rhoncus porta. Ut quis sem quis purus lobortis dictum.
+          Aliquam nec dignissim nisl. Vivamus cursus feugiat sapien, eget
+          tincidunt leo ornare quis..
+        </p>
+        <div className="buttons">
+          <button id="read">READ MORE</button>
+        </div>
+      </section>
+    </div>
+  );
+}
+export default Section1;
