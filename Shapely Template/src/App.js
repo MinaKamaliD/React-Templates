92f1ccf2d52ce2f29c0e9c77daa377ad6b9bce30
Mina <minakamali.d@gmail.com>
2022-11-17 18:35:04 +0330

    Section5 modified

--- Shapely Template/src/App.js
+++ Shapely Template/src/App.js
@@ -4,6 +4,7 @@ import Section1 from "./Section1/Section1";
 import Section2 from "./Section2/Section2";
 import Section3 from "./Section3/Section3";
 import Section4 from "./Section4/Section4";
+import Section5 from "./Section5/Section5";
 function App() {
   return (
     <div>
@@ -12,6 +13,7 @@ function App() {
       <Section2></Section2>
       <Section3></Section3>
       <Section4></Section4>
+      <Section5></Section5>
     </div>
   );
 }
---
+++ Shapely Template/src/Section5/Section5.js
@@ -0,0 +1,18 @@
+import "./Section5.css";
+
+function Section5() {
+  return (
+    <div>
+      <div className="container5">
+        <h2>Limitless Options</h2>
+        <p>
+          Phasellus sed nisi ac dui interdum semper. Etiam consequat fermentum
+          sollicitudin. Fusce vulputate porta faucibus. Vivamus nulla tellus,
+          accumsan non efficitur id, pretium quis ante
+        </p>
+        <button>DOWNLOAD NOW</button>
+      </div>
+    </div>
+  );
+}
+export default Section5;
